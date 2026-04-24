3aa3f3942eff5a7939cffbc5abb29b6070be2760
Fabian Kirchweger <fabian@kirchweger.com>
2026-04-24 13:17:14 +0200

    🐛 Fix: Add System.Drawing back for Bitmap in ExportManager (Size already qualified)

--- src/FlipsiInk/ExportManager.cs
+++ src/FlipsiInk/ExportManager.cs
@@ -7,6 +7,8 @@
 #nullable enable
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
